5fed045e1838b33face7e706c864830a8254d728
Juanan Pereira <juanan.pereira@ehu.eus>
2024-08-21 11:11:07 +0200

    fix: Implement timezone-aware datetime handling in assays and params modules

--- snailz/params.py
+++ snailz/params.py
@@ -2,19 +2,19 @@
 
 from argparse import Namespace
 from dataclasses import dataclass, field
-from datetime import date, datetime
+from datetime import date, datetime, timezone
 from importlib.resources import files
 import json
 from pathlib import Path
 from typing import List
-
+import pytz
 
 # Use ISO date format.
 DATE_FORMAT = '%Y-%m-%d'
 
 # Start and end dates for experiemnts.
-DEFAULT_START_DATE = datetime.strptime('2023-11-01', DATE_FORMAT)
-DEFAULT_END_DATE = datetime.strptime('2023-11-10', DATE_FORMAT)
+DEFAULT_START_DATE = datetime.strptime('2023-11-01', DATE_FORMAT).replace(tzinfo=pytz.UTC)
+DEFAULT_END_DATE = datetime.strptime('2023-11-10', DATE_FORMAT).replace(tzinfo=pytz.UTC)
 
 # Parameter files to include in package.
 PARAMETER_FILES = (
@@ -38,13 +38,13 @@ class AssayParams:
     assay_plates: list
     control_val: float = 5.0
     controls: List[str] = field(default_factory=list)
-    enddate: date = None
+    enddate: str = None
     experiments: int = 1
     filename_length: int = 8
     fraction: float = None
     invalid: float = 0.1
     seed: int = None
-    startdate: date = None
+    startdate: str = None
     stdev: float = 3.0
     treated_val: float = 8.0
     treatment: str = None
@@ -53,13 +53,17 @@ class AssayParams:
         '''Fill in missing dates and convert to standard format.'''
         if self.startdate is None:
             self.startdate = DEFAULT_START_DATE
-        else:
-            self.startdate = datetime.strptime(self.startdate, DATE_FORMAT)
+        elif isinstance(self.startdate, str):
+            self.startdate = datetime.strptime(self.startdate, DATE_FORMAT).replace(tzinfo=pytz.UTC)
+        elif not self.startdate.tzinfo:
+            self.startdate = self.startdate.replace(tzinfo=pytz.UTC)
 
         if self.enddate is None:
             self.enddate = DEFAULT_END_DATE
-        else:
-            self.enddate = datetime.strptime(self.enddate, DATE_FORMAT)
+        elif isinstance(self.enddate, str):
+            self.enddate = datetime.strptime(self.enddate, DATE_FORMAT).replace(tzinfo=pytz.UTC)
+        elif not self.enddate.tzinfo:
+            self.enddate = self.enddate.replace(tzinfo=pytz.UTC)
 
 
 @dataclass
